f2513ba75200d6381615e87b6b56bf8a19973f22
daniilsumilkin <danil.shumilkin.96@gmail.com>
2024-06-22 13:33:17 +0300

    clean

--- src/helpers/object.ts
+++ src/helpers/object.ts
@@ -1,4 +1,21 @@
-import { isEmpty } from "lodash-es";
+import { isEmpty, transform } from "lodash-es";
+import { isDate } from "date-fns";
+import { unref } from "vue";
+
+import type { ComputedRef, MaybeRef } from "vue";
+
+import { isFile } from "@/helpers/getters";
+
+type DeepUnref<
+  V,
+  T = V extends MaybeRef<infer R> | ComputedRef<infer R> ? R : V
+  > = T extends Array<infer E>
+  ? Array<DeepUnref<E>>
+  : T extends (...args: any[]) => any
+    ? T
+    : T extends Record<string, any>
+      ? { [Key in keyof T]: DeepUnref<T[Key]> }
+      : T;
 
 export const compactObject = <O extends Record<string, unknown>>(
   object: O
@@ -21,3 +38,21 @@ export const compactObject = <O extends Record<string, unknown>>(
 
 export const isObject = (value: unknown) =>
   value !== null && typeof value === "object" && !Array.isArray(value);
+
+
+export const deepUnref = <T>(value: T): DeepUnref<T> => {
+  const $value = unref(value);
+
+  if (Array.isArray($value))
+    return $value.map((item) => deepUnref(item)) as any;
+
+  if (
+    typeof $value === "object" &&
+    $value !== null &&
+    !isFile($value) &&
+    !isDate($value)
+  )
+    return transform($value, (value) => deepUnref(value)) as any;
+
+  return $value as any;
+};
